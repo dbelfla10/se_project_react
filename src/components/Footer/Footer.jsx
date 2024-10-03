c4255c88b77bd824317ce17afdde3ebde2803461
Diego <db_elfla10@outlook.es>
2024-10-03 16:44:37 -0400

    Fix styles for Main and Footer components

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__container">
+        <p className="footer__author">Developed by Diego Barrios</p>
+        <p className="footer__date">2024</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
