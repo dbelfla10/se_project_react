3b2247544a0ceab79f2f504c411c370b6ab98a69
Diego <db_elfla10@outlook.es>
2024-10-04 11:52:43 -0400

    Add ModalWithForm and styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
 
 function App() {
   const [weatherData, setWeatherData] = useState({ type: "cold" });
@@ -15,6 +16,44 @@ function App() {
         <Main weatherData={weatherData} />
         <Footer />
       </div>
+      <ModalWithForm title="New garment" buttonText="Add garment">
+        <label htmlFor="name" className="modal__label">
+          Name{""}
+          <input
+            type="text"
+            className="modal__input"
+            id="name"
+            placeholder="Name"
+          />
+        </label>
+        <label htmlFor="ImageUrl" className="modal__label">
+          Image{""}
+          <input
+            type="text"
+            className="modal__input"
+            id="ImageUrl"
+            placeholder="Image URL"
+          />
+        </label>
+        <fieldset className="modal__radio-buttons">
+          <legend className="modal__legend">Select the weather type:</legend>
+          <label htmlFor="hot" className="modal__label modal__label_type_radio">
+            <input id="hot" type="radio" className="modal__radio-input" /> Hot
+          </label>
+          <label
+            htmlFor="warm"
+            className="modal__label modal__label_type_radio"
+          >
+            <input id="warm" type="radio" className="modal__radio-input" /> Warm
+          </label>
+          <label
+            htmlFor="cold"
+            className="modal__label modal__label_type_radio"
+          >
+            <input id="cold" type="radio" className="modal__radio-input" /> Cold
+          </label>
+        </fieldset>
+      </ModalWithForm>
     </div>
   );
 }
---
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -0,0 +1,23 @@
+import "./ModalWithForm.css";
+import closeIcon from "../../assets/close-icon.svg";
+
+function ModalWithForm({ children, title, buttonText }) {
+  return (
+    <div className="modal">
+      <div className="modal__content">
+        <h2 className="modal__title">{title}</h2>
+        <button type="button" className="modal__close">
+          <img src={closeIcon} alt="close" className="modal__close-icon" />
+        </button>
+        <form className="modal__form">
+          {children}
+          <button type="submit" className="modal__submit">
+            {buttonText}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default ModalWithForm;
