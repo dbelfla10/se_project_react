dcf52e2ecdd1729c3f207b6e6258ab522211a46b
Diego <db_elfla10@outlook.es>
2024-11-19 12:13:23 -0500

    Add code to display current user's info

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,11 +1,18 @@
+import { useContext, useState } from "react";
+
 import "./SideBar.css";
-import avatar from "../../assets/avatar.png";
+// import avatar from "../../assets/avatar.png";
+import CurrentUserContext from "../../context/CurrentUserContext";
+import Avatar from "../Avatar/Avatar";
 
 const SideBar = () => {
+  const currentUser = useContext(CurrentUserContext);
+  const isSidebar = useState(true);
   return (
     <div className="sidebar">
-      <img src={avatar} alt="Default Avatar" className="sidebar__avatar" />
-      <p className="sidebar__username">Terrence Tegegne</p>
+      {/* <img src={avatar} alt="Default Avatar" className="sidebar__avatar" /> */}
+      <Avatar isSidebar={isSidebar} />
+      <p className="sidebar__username">{currentUser?.name}</p>
     </div>
   );
 };
